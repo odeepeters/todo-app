f51477ea8d2c6c467e1a71b19c27a3ab7413e39e
odeepeters <odeepeters@gmail.com>
2024-09-18 13:27:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 // src/App.jsx
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+// import AddTask from './components/AddTask';
 import TaskList from './components/TaskList';
-import './App.css';
 
 const App = () => {
-  return (
-    <div className="app">
-      <h1>To-Do List</h1>
-      <TaskList />
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <div>
+                <h1>ToDo Application</h1>
+                {/* <AddTask /> */}
+                <TaskList />
+            </div>
+        </Provider>
+    );
 };
 
 export default App;
+
+
+// src/App.jsx
+// import React from 'react';
+// import TaskList from './components/TaskList';
+// import './App.css';
+
+// const App = () => {
+//   return (
+//     <div className="app">
+//       <h1>To-Do List</h1>
+//       <TaskList />
+//     </div>
+//   );
+// };
+
+// export default App;
+
